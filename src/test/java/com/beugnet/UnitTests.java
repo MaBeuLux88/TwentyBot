d1b78fab67dc118891657c9f4d4fa764eb73531c
Maxime Beugnet <maxime.beugnet@zenika.com>
2016-05-16 01:25:09 +0200

    get correct colors from screenshot

--- src/test/java/com/beugnet/UnitTests.java
+++ src/test/java/com/beugnet/UnitTests.java
@@ -7,6 +7,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +19,7 @@ public class UnitTests {
     private static final int CORNER_Y = 10;
     private static final int WIDTH = 570;
     private static final int HEIGHT = 870;
+    private Map<Integer, Integer> colorMapping = new HashMap<>();
 
     private void robo() throws AWTException, IOException {
         Robot robot = new Robot();
@@ -27,7 +31,38 @@ public class UnitTests {
 
     @Test
     public void testScreenshot() throws IOException, AWTException {
-        BufferedImage screeShot = ImageIO.read(this.getClass().getResourceAsStream("test-input.png"));
-        assertThat(screeShot).isNotNull();
+
+        initMap();
+
+        InputStream resourceAsStream = UnitTests.class.getClassLoader().getResourceAsStream("test-input.png");
+        BufferedImage screenShot = ImageIO.read(resourceAsStream);
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println("\nLIGNE " + (i + 1));
+            for (int j = 0; j < 7; j++) {
+                int rgb = screenShot.getRGB(20 + 80 * j, 822 - 85 * i);
+                System.out.print((colorMapping.get(rgb) != null ? colorMapping.get(rgb) : rgb )+ "\t");
+            }
+        }
+
+        assertThat(screenShot).as("screenShot null").isNotNull();
+    }
+
+    private void initMap() {
+        colorMapping.put(-2170914, 0);
+        colorMapping.put(-100, 1);
+        colorMapping.put(-56287, 2);
+        colorMapping.put(-16714835, 3);
+        colorMapping.put(-14054657, 4);
+        colorMapping.put(-2185473, 5);
+        colorMapping.put(-13505792, 6);
+        colorMapping.put(-11331, 7);
+        colorMapping.put(-6553353, 8);
+        colorMapping.put(-18944, 9);
+        colorMapping.put(-3750970, 10);
+        colorMapping.put(-3212544, 11);
+        colorMapping.put(-256, 12);
+        colorMapping.put(-59269, 13);
+        colorMapping.put(-8093052, 15);
     }
 }
